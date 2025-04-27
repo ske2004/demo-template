1592c5c2ddfccc9d92a66a2c84d26fd2b2d6035a
ske2004 <contact@ske.land>
2025-04-27 18:21:01 -0300

    CI

--- build.bat
+++ build.bat
@@ -1,4 +1,17 @@
+@echo off
+
+set CRINKLER_EXE=C:\crinkler\crinkler23\Win32\crinkler.exe
+if not exist %CRINKLER_EXE% (
+    set CRINKLER_EXE=crinkler.exe
+)
+
 if not exist bin mkdir bin
-cd bin
-cl ../main.c /O1 /c /GS- && crinkler main.obj user32.lib kernel32.lib gdi32.lib dsound.lib /SUBSYSTEM:WINDOWS /NODEFAULTLIB
-cd ..
+
+echo ^>Bob the builder, can we build it?
+
+cl main.c /O1 /c /GS- || exit /b
+%CRINKLER_EXE% main.obj user32.lib kernel32.lib gdi32.lib dsound.lib /SUBSYSTEM:WINDOWS /NODEFAULTLIB /out:bin\demo.exe || exit /b
+
+del *.obj
+
+echo ^>Bob the builder, yes we can! (Tutututu)
